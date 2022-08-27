7e0feedb473c067d95479a1f0ef219ba9c3024f8
Dmitry Säkki <dmitry.sakki@edu.sasky.fi>
2022-08-27 19:39:59 +0300

    sähköpostiosoitteen varauksen tarkistus

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -6,5 +6,8 @@
     DB::run('INSERT INTO henkilo (nimi, email, salasana) VALUE  (?,?,?);',[$nimi,$email,$salasana]);
     return DB::lastInsertId();
   }
+  function haeHenkiloSahkopostilla($email) {
+    return DB::run('SELECT * FROM henkilo WHERE email = ?;', [$email])->fetchAll();
+  }
 
 ?>
