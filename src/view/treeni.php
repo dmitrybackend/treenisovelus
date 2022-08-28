0c7f50e807c310568c4f6649bd301d93bca90304
Dmitry Säkki <dmitry.sakki@edu.sasky.fi>
2022-08-28 15:39:59 +0300

    ilmoittaudu-nappi tapahtumasivulle

--- src/view/treeni.php
+++ src/view/treeni.php
@@ -9,3 +9,15 @@
 <div><?=$treeni['kuvaus']?></div>
 <div>Alkaa: <?=$start->format('j.n.Y G:i')?></div>
 <div>Loppuu: <?=$end->format('j.n.Y G:i')?></div>
+<?php
+  if ($loggeduser) {
+    if (!$ilmoittautuminen) {
+      echo "<div class='flexarea'><a href='ilmoittaudu?id=$treeni[idtreeni]' class='button'>ILMOITTAUDU</a></div>";    
+    } else {
+      echo "<div class='flexarea'>";
+      echo "<div>Olet ilmoittautunut treeniin!</div>";
+      echo "<a href='peru?id=$treeni[idtreeni]' class='button'>PERU ILMOITTAUTUMINEN</a>";
+      echo "</div>";
+    }
+  }
+?>
